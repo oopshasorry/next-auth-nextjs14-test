a507b9aa30f144e5ba360d3d3847c365062c341e
Joel <joel.patane@rib-software.com>
2023-12-21 09:53:24 +1000

    back

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -8,11 +8,11 @@ import AzureADB2C from "next-auth/providers/azure-ad-b2c";
 
 
 const credential = new AzureNamedKeyCredential(
-    ""+ process.env.AZURE_ACCOUNT as string,
-    ""+ process.env.AZURE_ACCESS_KEY as string
+    process.env.AZURE_ACCOUNT as string,
+    process.env.AZURE_ACCESS_KEY as string
 );
 const authClient = new TableClient(
-    ""+process.env.AZURE_TABLES_ENDPOINT as string,
+    process.env.AZURE_TABLES_ENDPOINT as string,
     "auth",
     credential
 );
@@ -22,25 +22,25 @@ export const authOptions ={
     
     providers: [
         GitHubProvider({
-            clientId: ""+process.env.GITHUB_ID as string,
-            clientSecret: ""+process.env.GITHUB_SECRET_ID as string,
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET_ID as string,
         }),
         EmailProvider({
             server: {
-                host: ""+process.env.EMAIL_SERVER_HOST,
-                port: ""+process.env.EMAIL_SERVER_PORT,
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
                 auth: {
-                user: ""+process.env.EMAIL_SERVER_USER,
-                pass: ""+process.env.EMAIL_SERVER_PASSWORD
+                user: process.env.EMAIL_SERVER_USER,
+                pass: process.env.EMAIL_SERVER_PASSWORD
                 }
             },
             from: process.env.EMAIL_FROM
         }),
         AzureADB2C({
-            tenantId: ""+process.env.AZURE_AD_B2C_TENANT_NAME,
-            clientId: ""+process.env.AZURE_AD_B2C_CLIENT_ID as string,
-            clientSecret: ""+process.env.AZURE_AD_B2C_CLIENT_SECRET as string,
-            primaryUserFlow: ""+process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW,
+            tenantId: process.env.AZURE_AD_B2C_TENANT_NAME,
+            clientId: process.env.AZURE_AD_B2C_CLIENT_ID as string,
+            clientSecret: process.env.AZURE_AD_B2C_CLIENT_SECRET as string,
+            primaryUserFlow: process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW,
             authorization: { params: { scope: "offline_access openid" } },
             checks:["pkce"],
             client: {
