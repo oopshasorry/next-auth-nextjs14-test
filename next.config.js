166fc788534b0e7559e319d6c97bdd4104f3856b
Joel <joel.patane@rib-software.com>
2024-01-08 15:04:52 +1000

    security headers v1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,53 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: "standalone"}
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
+const nextConfig = {
+    
+    output: "standalone",
+    async headers() {
+        return [
+            {
+            source: '/(.*)',
+            headers: [
+                {
+                key: 'Content-Security-Policy',
+                value: cspHeader.replace(/\n/g, ''),
+                },
+                {
+                key: 'X-Frame-Options',
+                value: 'SAMEORIGIN'
+                },
+                {
+                key: 'Referrer-Policy',
+                value: 'origin-when-cross-origin'
+                },
+                {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff'
+                },
+                {
+                key: 'Strict-Transport-Security',
+                value: 'max-age=63072000; includeSubDomains; preload'
+                }
 
+
+                ],
+            },
+        ]
+
+    },
+    poweredByHeader: false,
+}
+  
 module.exports = nextConfig
